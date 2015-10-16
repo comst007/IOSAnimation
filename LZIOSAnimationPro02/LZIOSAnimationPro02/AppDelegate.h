0e188650f7c3fc0486a28d33324b51170ef84dbf
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-10-17 01:26:55 +0800

    侧滑菜单 动画

---
+++ LZIOSAnimationPro02/LZIOSAnimationPro02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZIOSAnimationPro02
+//
+//  Created by comst on 15/10/17.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZIOSAnimationPro02/LZIOSAnimationPro02/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LZIOSAnimationPro02
+//
+//  Created by comst on 15/10/17.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
